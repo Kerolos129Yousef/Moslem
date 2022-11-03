2c901413d1b19e031efbaf043289b54f7546416e
kerolos129Yousef <kerolos.yousef129@gmail.com>
2022-11-03 14:17:31 +0200

    initialize project added empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:moslem/Home_Screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen()
+      },
+    );
+  }
+}
+
