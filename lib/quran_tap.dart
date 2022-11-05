e2f1c76f1b627f75a83a96f22cf87c9585f9f4ca
kerolos129Yousef <kerolos.yousef129@gmail.com>
2022-11-06 01:06:53 +0200

    update Bottom Navbar and initalize all taps

---
+++ lib/quran_tap.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class QuranTap extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.brown,
+    );
+  }
+}
---
+++ lib/tasbeh_tap.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class TasbehTap extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
